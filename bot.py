2e7a56e4026dec6c2e2f9f0d11dcadfbbda1f7ec
Mauricio Blanco Regojo <mauricioblancoregojo@gmail.com>
2022-04-14 20:06:05 -0300

    updated fetching miner info from api

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import asyncio
+import datetime
 import re
 import requests
 import telepot
@@ -8,36 +9,44 @@ from bs4 import BeautifulSoup
 from dotenv import dotenv_values
 from pprint import pprint
 from telepot.aio.loop import MessageLoop
-from requests_html import HTMLSession
-from fake_useragent import UserAgent
 
 config = dotenv_values('.env')
 
-ethermine_url = f'https://ethermine.org/miners/{config["ADDRESS"]}/dashboard'
+ethermine_url = f'https://api.ethermine.org/miner/{config["ADDRESS"]}/dashboard'
 dictionary_url = 'https://www.oxfordlearnersdictionaries.com/definition/english/'
 
-session = HTMLSession()
-
 async def handle(msg):
-	async def commands(command, text):
+	async def commands(text):
 		help = """
-			/help
-			/dictionary {text}
-			/unpaid
-			/payday
-			/hashrate
+		Commands:
+		/start
+		/help
+		/dictionary {text}
+		/money
+		♬ ♪ ♮♫♭♩
+		Never gonna give you up
+		Never gonna let you down
+		Never gonna run around and desert you
+		Never gonna make you cry
+		Never gonna say goodbye
+		Never gonna tell a lie and hurt you 
+		♬ ♪ ♮♫♭♩
 		"""
+		command_list = re.findall(r'(/[A-Za-z]+)', text)
+
+		try:
+			command=command_list[0]
+		except IndexError:
+			command = ''
+
 		text = text.replace(command, '').strip().lower()
 		switcher = {
-			'/start':await bot.sendMessage(chat_id, 'Hi buddy'),
-			'/help':await bot.sendMessage(chat_id, help),
-			'/dictionary':await getMeaning(text),
-			'/unpaid':await bot.sendMessage(chat_id, 'unpaid'),
-			'/payday':await bot.sendMessage(chat_id, 'payday'),
-			'/hashrate':await bot.sendMessage(chat_id, 'hashrate'),
+			'/start': lambda: bot.sendMessage(chat_id, 'Hi buddy'),
+			'/help': lambda: bot.sendMessage(chat_id, help),
+			'/dictionary': lambda: getMeaning(text),
+			'/money': lambda: getMinerData(),
 		}
-		pprint(text, 'text')
-		return switcher.get(command.strip().lower(), await bot.sendMessage(chat_id, help))
+		return await switcher.get(command.strip().lower(), lambda: bot.sendMessage(chat_id, help))()
 
 	global chat_id
 	# These are some useful variables
@@ -45,6 +54,7 @@ async def handle(msg):
 	# Log variables
 	print(content_type, chat_type, chat_id)
 	pprint(msg)
+	global username
 	username = msg['chat']['first_name']
 	# Check that the content type is text and not the starting
 	if content_type == 'text':
@@ -52,23 +62,30 @@ async def handle(msg):
 		await commands(text)
 
 
-async def getHashrate():
-	# create url
-	r = session.get(ethermine_url)
-	r.html.render()
-	# pprint(r.html)
-	# r.html.search('Python 2 will retire in only {months} months!')['months']
+def fetchEthermineData():
+	try:
+		r = requests.get(ethermine_url)
+		data = r.json()['data']['currentStatistics']
+		return dict(data), r.status_code
+	except:
+		return None, r.status_code
 
-	# # let's soup the page
-	# soup = BeautifulSoup(page.text, 'html.parser')
-	# pprint(soup)
-	# try:
-	# 	# get Hashrate
-	# 	definition = soup.find('span', {'class': 'def'}).text
-	# 	await bot.sendMessage(chat_id, definition)
-	# except:
-	# 	await bot.sendMessage(chat_id, 'Something went wrong...')
 
+async def getMinerData():
+	data, status = fetchEthermineData()
+	if data:
+		response = f"""Hello {username}!
+		Reporting...
+		active workers: {data['activeWorkers']}
+		current hashrate: {data['currentHashrate']/1_000_000} MHz
+		reported hashrate: {data['reportedHashrate']/1_000_000} MHz
+		lastSeen: {datetime.datetime.utcfromtimestamp(data['lastSeen']).isoformat()}
+		unpaid: {data['unpaid']/10**18} ethers
+		Goodbye...
+		""" 
+	else:
+		response = f"Error {status}"
+	await bot.sendMessage(chat_id, response)
 
 async def getMeaning(text):
 	# create url
